b324af481eb8f1a179c374ba4bc7c558f4eb1436
Sreecharan-Akireddy <40417328+Sreecharan-Akireddy@users.noreply.github.com>
2020-10-15 20:55:40 +0530

    selecting team for upcoming matches

--- src/Components/UpcomingMatches/UpcomingMatches.js
+++ src/Components/UpcomingMatches/UpcomingMatches.js
@@ -7,16 +7,22 @@ class UpcomingMatches extends Component{
     constructor(props){
         super(props)
         this.state = {"matches":
-        {"51": {"team1": "SRH", "team2": "MI", "venue": "Sharjah", "day": "12th Oct 2020, Monday"},
-        "52": {"team1": "RCB", "team2": "RR", "venue": "Abu dhabi", "day": "13th Oct 2020, Tuesday"},
-        "53": {"team1": "KKR", "team2": "CSK", "venue": "Dubai", "day": "14th Oct 2020, Wednesday"},
-        "54": {"team1": "KXIP", "team2": "DC", "venue": "Abu dhabi", "day": "15th Oct 2020, Thursday"},
-        "55": {"team1": "RR", "team2": "KKR", "venue": "Dubai", "day": "16th Oct 2020, Friday"},
-        "56": {"team1": "CSK", "team2": "KXIP", "venue": "Abu dhabi", "day": "17th Oct 2020, Saturday"},
-        "57": {"team1": "SRH", "team2": "RCB", "venue": "Dubai", "day": "18th Oct 2020, Sunday"},
-        "58": {"team1": "MI", "team2": "DC", "venue": "Abu dhabi", "day": "19th Oct 2020, Monday"}
+        {"51": {"team1": "SRH", "team2": "MI", "venue": "Sharjah", "day": "12th Oct 2020, Monday", "teamSelected" :""},
+        "52": {"team1": "RCB", "team2": "RR", "venue": "Abu dhabi", "day": "13th Oct 2020, Tuesday", "teamSelected" :""},
+        "53": {"team1": "KKR", "team2": "CSK", "venue": "Dubai", "day": "14th Oct 2020, Wednesday", "teamSelected" :""},
+        "54": {"team1": "KXIP", "team2": "DC", "venue": "Abu dhabi", "day": "15th Oct 2020, Thursday", "teamSelected" :""},
+        "55": {"team1": "RR", "team2": "KKR", "venue": "Dubai", "day": "16th Oct 2020, Friday", "teamSelected" :""},
+        "56": {"team1": "CSK", "team2": "KXIP", "venue": "Abu dhabi", "day": "17th Oct 2020, Saturday", "teamSelected" :""},
+        "57": {"team1": "SRH", "team2": "RCB", "venue": "Dubai", "day": "18th Oct 2020, Sunday", "teamSelected" :""},
+        "58": {"team1": "MI", "team2": "DC", "venue": "Abu dhabi", "day": "19th Oct 2020, Monday", "teamSelected" :""}
             }
         }
+    }
+
+    selectTeam(match, team){
+        const newMatches = {...this.state.matches};
+        newMatches[match].teamSelected = team
+        this.setState({matches: newMatches})
     }
     render(){
         let upcomingMatchesCards = Object.entries(this.state.matches).map(([matchid, value]) => {
@@ -26,8 +32,12 @@ class UpcomingMatches extends Component{
                 <h5 className="card-title">{value.day}</h5>
                 <p className="card-text">{value.venue}</p>
                 <div className="btn-group btn-block">
-                        <Button className="btn-match card">{value.team1}</Button>
-                        <Button className="btn-match card">{value.team2}</Button>
+                        <Button className="btn-match card" onClick={() =>this.selectTeam(matchid, value.team1)}>
+                            {(value.team1 === value.teamSelected) ?  value.team1 + " ✓" : value.team1}
+                            </Button>
+                        <Button className="btn-match card" onClick={() =>this.selectTeam(matchid, value.team2)}>
+                            {(value.team2 === value.teamSelected) ?  value.team2 + " ✓" : value.team2}
+                            </Button>
                     </div>
                 </div>
                 </div>
